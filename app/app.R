3535139786f7c783b3353325d56924d79e2d48ae
Eric Scott <scottericr@gmail.com>
2026-02-04 11:40:01 -0700

    try "manual" pwa

--- app/app.R
+++ app/app.R
@@ -15,13 +15,14 @@ ui <-
     navsetCardTab = bslib::page(
       title = NULL,
       theme = theme, # `scr##_theme.R`
-      shiny.pwa::pwa(
-        "https://viz.datascience.arizona.edu/azmet/azmet-in-real-time/",
-        title = "AZMet",
-        # TODO: replace with block A icon?  Can also be a path to a local file. E.g. "wws/icon.png"
-        icon = "https://github.com/uace-azmet/azmetr/blob/21f4b54f2b1d050fe75d191268274508d95071b6/pkgdown/favicon/apple-touch-icon.png",
-        output = "www"
-      ),
+      tags$head(includeHTML("www/pwa.html")),
+      # shiny.pwa::pwa(
+      #   "https://viz.datascience.arizona.edu/azmet/azmet-in-real-time/",
+      #   title = "AZMet",
+      #   # TODO: replace with block A icon?  Can also be a path to a local file. E.g. "wws/icon.png"
+      #   icon = "https://github.com/uace-azmet/azmetr/blob/21f4b54f2b1d050fe75d191268274508d95071b6/pkgdown/favicon/apple-touch-icon.png",
+      #   output = "www"
+      # ),
       bslib::navset_card_tab(
         id = "navsetCardTab",
         selected = "network-wide-summary",
